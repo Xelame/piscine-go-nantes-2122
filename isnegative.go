b0f7a9771660c00d74676fafcaefed81d6665847
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-14 14:42:06 +0200

    adaptation au test v8

--- isnegative.go
+++ isnegative.go
@@ -5,13 +5,15 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
+		z01.PrintRune('\n')
 	} else {
 		z01.PrintRune('F')
+		z01.PrintRune('\n')
 	}
 }
 
 func main() {
 	IsNegative(1)
-	IsNegativ(0)
+	IsNegative(0)
 	IsNegative(-1)
 }
