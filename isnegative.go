dd67e145d647087f14f5c94b4bea948c239beae7
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-14 15:12:52 +0200

    rework with loop

--- isnegative.go
+++ isnegative.go
@@ -3,8 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-
+	
 }
+
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
@@ -14,3 +15,4 @@ func IsNegative(nb int) {
 		z01.PrintRune('\n')
 	}
 }
+
