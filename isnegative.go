53ac4fabbf22eef1882455021328ba27ed4ef837
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-14 14:22:40 +0200

    Adaptation au test v3

--- isnegative.go
+++ isnegative.go
@@ -2,13 +2,13 @@ package main
 
 import ("github.com/01-edu/z01")
 
-func main() {
-}
-
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
 	} else {
 		z01.PrintRune('F')
 	}
+}
+
+func main() {
 }
