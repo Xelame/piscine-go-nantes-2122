8711b9c1cc336c417e16697c5d6345ad947e79a8
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-14 16:12:52 +0200

    adaptation au test v12

--- isnegative.go
+++ isnegative.go
@@ -1,9 +1,13 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
-import "piscine"
 
-func main() {
-
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('T')
+		z01.PrintRune('\n')
+	} else {
+		z01.PrintRune('F')
+		z01.PrintRune('\n')
+	}
 }
-
