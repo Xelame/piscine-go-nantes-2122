2d218010b083e5f3ab1bb350e75de7c5df4a431b
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-15 18:22:57 +0200

    test without test

--- printcomb2.go
+++ printcomb2.go
@@ -13,22 +13,16 @@ func PrintComb2() {
 			for tens1 := '0'; tens1 <= '9'; tens1++ {
 				for units1 := '0'; units1 <= '9'; units1++ {
 					// Testing if they're in ascending order
-					if units2 <= units1 && tens2 <= tens1 {
-						if units1 == '0' && units2 == '0' && tens1 == '0' && tens2 == '0' {
-							units1 = '0'
-						} else {
-							z01.PrintRune(tens2)
-							z01.PrintRune(units2)
-							z01.PrintRune(' ')
-							z01.PrintRune(tens1)
-							z01.PrintRune(units1)
-							if units1 == '9' && tens1 == '9' && units2 == '8' && tens2 == '9' {
-								z01.PrintRune('\n')
-							} else {
-								z01.PrintRune(',')
-								z01.PrintRune(' ')
-							}
-						}
+					z01.PrintRune(tens2)
+					z01.PrintRune(units2)
+					z01.PrintRune(' ')
+					z01.PrintRune(tens1)
+					z01.PrintRune(units1)
+					if units1 == '9' && tens1 == '9' && units2 == '8' && tens2 == '9' {
+						z01.PrintRune('\n')
+					} else {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 					}
 				}
 			}
