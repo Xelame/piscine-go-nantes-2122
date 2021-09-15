a6b8aa9c2c0cfe14becd8225b4cdc2dd7fc9b2c1
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-15 18:47:27 +0200

    adjust my testing

--- printcomb2.go
+++ printcomb2.go
@@ -13,16 +13,18 @@ func PrintComb2() {
 			for tens1 := '0'; tens1 <= '9'; tens1++ {
 				for units1 := '0'; units1 <= '9'; units1++ {
 					// Testing if they're in ascending order
-					z01.PrintRune(tens2)
-					z01.PrintRune(units2)
-					z01.PrintRune(' ')
-					z01.PrintRune(tens1)
-					z01.PrintRune(units1)
-					if units1 == '9' && tens1 == '9' && units2 == '8' && tens2 == '9' {
-						z01.PrintRune('\n')
-					} else {
-						z01.PrintRune(',')
+					if tens1 > tens2 || (tens1 == tens2 && units1 > units2) {
+						z01.PrintRune(tens2)
+						z01.PrintRune(units2)
 						z01.PrintRune(' ')
+						z01.PrintRune(tens1)
+						z01.PrintRune(units1)
+						if units1 == '9' && tens1 == '9' && units2 == '8' && tens2 == '9' {
+							z01.PrintRune('\n')
+						} else {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 					}
 				}
 			}
