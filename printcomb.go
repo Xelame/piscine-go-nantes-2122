bc4f2a33d5f07453f6ab91bb559d7d2e754da98a
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-15 15:22:13 +0200

    formatage v1

--- printcomb.go
+++ printcomb.go
@@ -7,20 +7,20 @@ Function to print all unique combinations of three different digit in ascending 
 Output : Suit of answers separate by a comma
 */
 func PrintComb() {
-        // Generator for our three different digits
-        for hundreds := '0'; hundreds <= '9'; hundreds++ {
-                for tens := '0'; tens <= '9'; tens++ {
-                        for units := '0'; units <= '9'; units++ {
+	// Generator for our three different digits
+	for hundreds := '0'; hundreds <= '9'; hundreds++ {
+		for tens := '0'; tens <= '9'; tens++ {
+			for units := '0'; units <= '9'; units++ {
 
-                                // Testing if they're in ascending order
-                                if units < tens && tens < hundreds {
-                                        z01.PrintRune(hundreds)
-                                        z01.PrintRune(tens)
-                                        z01.PrintRune(units)
-                                        z01.PrintRune(',')
-                                        z01.PrintRune(' ')
-                                }
-                        }
-                }
-        }
+				// Testing if they're in ascending order
+				if units < tens && tens < hundreds {
+					z01.PrintRune(hundreds)
+					z01.PrintRune(tens)
+					z01.PrintRune(units)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
 }
