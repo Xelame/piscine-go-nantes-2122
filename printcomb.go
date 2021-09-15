296dc246a68c1258ccf4ec21052f54c59ac9b491
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-15 15:17:49 +0200

    Comprehension up

--- printcomb.go
+++ printcomb.go
@@ -8,15 +8,15 @@ Output : Suit of answers separate by a comma
 */
 func PrintComb() {
 	// Generator for our three different digits
-	for units := '0'; units <= '9'; units++ {
+	for hundreds := '0'; hundreds <= '9'; hundreds++ {
 		for tens := '0'; tens <= '9'; tens++ {
-			for hundreds := '0'; hundreds <= '9'; hundreds++ {
+			for units := '0'; units <= '9'; units++ {
 
 				// Testing if they're in ascending order
 				if units < tens && tens < hundreds {
-					z01.PrintRune(units)
-					z01.PrintRune(tens)
 					z01.PrintRune(hundreds)
+					z01.PrintRune(tens)
+					z01.PrintRune(units)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
