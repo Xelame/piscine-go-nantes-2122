e5deff6889d6c9d3e28651b0202bb51127e7a857
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-15 10:35:29 +0200

    adaptation au test v1

--- printcomb.go
+++ printcomb.go
@@ -11,7 +11,7 @@ func PrintComb() {
 	for units := '0'; units <= '9'; units++ {
 		for tens := '0'; tens <= '9'; tens++ {
 			for hundreds := '0'; hundreds <= '9'; hundreds++ {
-
+				
 				// Testing if they're in ascending order
 				if units < tens && tens < hundreds {
 					z01.PrintRune(units)
