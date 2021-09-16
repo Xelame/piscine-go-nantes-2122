7b4b85a44dd0b323fc00fe6ee15315c0bdaad3a0
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-16 09:16:32 +0200

    restart something

--- printnbr.go
+++ printnbr.go
@@ -3,18 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(number int) {
-	// if is a negative number
 	if number < 0 {
 		z01.PrintRune('-')
 		number -= 2 * number
 	}
-	// search the max power of 10 to read the first digit of number
 	for number > 10 {
 		power := 1
 		for number/power > 10 {
 			power *= 10
 		}
-		// search which digits is bitween 1 and 9
+
 		if number > power {
 			var count int32 = 1
 			for i := 1; i <= 9; i++ {
@@ -27,7 +25,6 @@ func PrintNbr(number int) {
 			}
 		}
 	}
-	// Same thing just for the last digit
 	if number > 0 {
 		var count int32 = 1
 		for i := 1; i <= 9; i++ {
