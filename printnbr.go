2c134e66fba0da213a74087b8e3814d554408cfc
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-16 15:00:29 +0200

    adjust 0 testing

--- printnbr.go
+++ printnbr.go
@@ -1,40 +1,43 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbr(number int) {
+	// Test if number is negative
 	if number < 0 {
 		z01.PrintRune('-')
 		number -= 2 * number
 	}
+	// Create a loop to set up my power of 10
 	for number > 10 {
 		power := 1
 		for number/power > 10 {
 			power *= 10
 		}
+		// init some variable
 		var count int32 = 1
-		testing := power
+		testing := power/10
+		// surrounding our digit * 1^power
 		for i := 1; i <= 9; i++ {
 			if i*power < number && number < (i+1)*power {
 				z01.PrintRune(48 + count)
-				for number%power < power/10/testing {
+				// test for the presence of one 0 or more
+				for number%power < testing {
 					z01.PrintRune(48)
-					testing *= 10
+					testing /= 10
 				}
+				// moving on to the digit in the right
 				number %= power
 			} else {
 				count++
 			}
 		}
 	}
+	// Testing delayed for the last digit (unit)
 	if number > 0 {
-		var count int32 = 1
-		for i := 1; i <= 9; i++ {
-			if i == number {
-				z01.PrintRune(48 + count)
-			} else {
-				count++
-			}
-		}
+		count := rune(number % 10)
+		z01.PrintRune(48+count)
 	}
 }
