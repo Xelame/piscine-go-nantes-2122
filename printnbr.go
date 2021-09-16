9a45d9ad98b6b7933ee2a3881e512e06f96f89bc
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-16 09:51:54 +0200

    change thing

--- printnbr.go
+++ printnbr.go
@@ -13,13 +13,13 @@ func PrintNbr(number int) {
 			power *= 10
 		}
 		var count int32 = 1
-		testing := 1
+		testing := power
 		for i := 1; i <= 9; i++ {
 			if i*power < number && number < (i+1)*power {
 				z01.PrintRune(48 + count)
-				for number%power < power/10*testing {
+				for number%power < power/10/testing {
 					z01.PrintRune(48)
-					testing *= 10
+					testing /= 10
 				}
 				number %= power
 			} else {
