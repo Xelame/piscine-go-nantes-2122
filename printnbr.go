ba7568d2c3892cc54f8830541cec54c7d77aa714
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-16 19:35:01 +0200

    Adjust to don't convert my negative number

--- printnbr.go
+++ printnbr.go
@@ -1,20 +1,25 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbr(number int) {
 	// Test if number is negative
 	if number < 0 {
 		z01.PrintRune('-')
-		number = -number
 	}
 	var reversedNumberInList []int
-	for number >= 10 {
+	for number >= 10 || number <= -10 {
 		reversedNumberInList = append(reversedNumberInList, number%10)
 		number /= 10
 	}
 	reversedNumberInList = append(reversedNumberInList, number%10)
 	for end := len(reversedNumberInList) - 1; end >= 0; end-- {
-		z01.PrintRune(rune(48 + reversedNumberInList[end]))
+		if reversedNumberInList[end] > 10 {
+			z01.PrintRune(rune(48 + reversedNumberInList[end]))
+		} else {
+			z01.PrintRune(rune(48 - reversedNumberInList[end]))
+		}
 	}
 }
