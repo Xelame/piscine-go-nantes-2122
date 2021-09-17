a5d16f486470299fc10f39d275dd867e7c8c39da
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-17 11:21:27 +0200

    go fmt

--- sortintegertable.go
+++ sortintegertable.go
@@ -2,7 +2,7 @@ package piscine
 
 func SortIntegerTable(table []int) {
 	for min_index := 0; min_index < len(table)-1; min_index++ {
-		for index := min_index+1; index < len(table); index++ {
+		for index := min_index + 1; index < len(table); index++ {
 			if table[index] < table[min_index] {
 				Swap(&table[index], &table[min_index])
 			}
