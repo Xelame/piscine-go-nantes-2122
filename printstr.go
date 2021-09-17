b05594cb4574e2449e7e31a0c0b7b4ef36629b3f
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-17 09:02:24 +0200

    variable more readable

--- printstr.go
+++ printstr.go
@@ -4,8 +4,8 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintStr(s string) {
-	for _, i := range s {
-		z01.PrintRune(i)
+func PrintStr(aString string) {
+	for _, letter := range aString {
+		z01.PrintRune(letter)
 	}
 }
