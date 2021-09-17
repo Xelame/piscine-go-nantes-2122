44f55ab40ceb446c971bab0c3a2804a4a7ba0c03
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-17 10:05:51 +0200

    if empty str

--- atoi.go
+++ atoi.go
@@ -1,20 +1,24 @@
 package piscine
 
 func Atoi(aString string) int {
-	powerOfTen := 1
-	result := 0
-	for index := range aString {
-		if 48 <= aString[len(aString)-1-index] && aString[len(aString)-1-index] <= 57 {
-			result += powerOfTen * int(aString[len(aString)-1-index]%48)
-			powerOfTen *= 10
-		} else if (aString[len(aString)-1-index] == 45 || aString[len(aString)-1-index] == 43) && len(aString)-1-index == 0 {
-			powerOfTen *= 10
-		} else {
-			return 0
+	if aString != "" {
+		powerOfTen := 1
+		result := 0
+		for index := range aString {
+			if 48 <= aString[len(aString)-1-index] && aString[len(aString)-1-index] <= 57 {
+				result += powerOfTen * int(aString[len(aString)-1-index]%48)
+				powerOfTen *= 10
+			} else if (aString[len(aString)-1-index] == 45 || aString[len(aString)-1-index] == 43) && len(aString)-1-index == 0 {
+				powerOfTen *= 10
+			} else {
+				return 0
+			}
 		}
+		if aString[0] == 45 {
+			result = -result
+		}
+		return result
+	} else {
+		return 0
 	}
-	if aString[0] == 45 {
-		result = -result
-	}
-	return result
 }
