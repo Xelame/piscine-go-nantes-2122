1eaec2c924cd0041d05a5d0208f92a15c391b1f4
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-17 08:59:57 +0200

    Remove Harcoding

--- printcomb.go
+++ printcomb.go
@@ -7,23 +7,24 @@ Function to print all unique combinations of three different digit in ascending 
 Output : Suit of answers separate by a comma
 */
 func PrintComb() {
+	isBeginning := true
 	// Generator for our three different digits
 	for hundreds := '0'; hundreds <= '7'; hundreds++ {
 		for tens := '0'; tens <= '8'; tens++ {
 			for units := '0'; units <= '9'; units++ {
 				// Testing if they're in ascending order
 				if units > tens && tens > hundreds {
-					z01.PrintRune(hundreds)
-					z01.PrintRune(tens)
-					z01.PrintRune(units)
-					if units == '9' && tens == '8' && hundreds == '7' {
-						z01.PrintRune('\n')
-					} else {
+					if !isBeginning {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
+					z01.PrintRune(hundreds)
+					z01.PrintRune(tens)
+					z01.PrintRune(units)
+					isBeginning = false
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
--- printcomb2.go
+++ printcomb2.go
@@ -7,6 +7,7 @@ Function to print all unique combinations of three different digit in ascending 
 Output : Suit of answers separate by a comma
 */
 func PrintComb2() {
+	isBeginning = true
 	// Generator for our 2 differents two-digits
 	for tens2 := '0'; tens2 <= '9'; tens2++ {
 		for units2 := '0'; units2 <= '9'; units2++ {
@@ -14,20 +15,20 @@ func PrintComb2() {
 				for units1 := '0'; units1 <= '9'; units1++ {
 					// Testing if they're in ascending order
 					if tens1 > tens2 || (tens1 == tens2 && units1 > units2) {
+						if !isBeginning {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 						z01.PrintRune(tens2)
 						z01.PrintRune(units2)
 						z01.PrintRune(' ')
 						z01.PrintRune(tens1)
 						z01.PrintRune(units1)
-						if units1 == '9' && tens1 == '9' && units2 == '8' && tens2 == '9' {
-							z01.PrintRune('\n')
-						} else {
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
-						}
+						isBeginning = false
 					}
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
