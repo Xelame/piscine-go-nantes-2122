dbbacb70d139e981082d25a336cae843032f4ebb
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-20 13:24:34 +0200

    adaptation

--- atoi.go
+++ atoi.go
@@ -3,7 +3,7 @@ package piscine
 func Atoi(aString string) int {
 	powerOfTen := 1
 	result := 0
-	for index := len(aString) - 1; index > 0; index-- {
+	for index := len(aString) - 1; index >= 0; index-- {
 		if 48 <= aString[index] && aString[index] <= 57 {
 			result += powerOfTen * int(aString[index]%48)
 			powerOfTen *= 10
