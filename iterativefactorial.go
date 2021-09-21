ae85e0c89f02de7418e7595575dd27fd87977fc8
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 13:37:44 +0200

    adjust limit loop

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,7 +3,7 @@ package piscine
 func IterativeFactorial(nb int) int {
 	result := 0
 	nb2 := nb - 1
-	for index := nb2; 9223372036854775807 > index && index > 0; index-- {
+	for index := nb2; 9223372036854775807 > nb && index > 0; index-- {
 		nb *= index
 		result = nb
 	}
