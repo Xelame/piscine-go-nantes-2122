e5bd2e65f265d00e480ccedb5ba90fb205a51bc1
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 14:44:49 +0200

    EX 2 Q4

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,10 +1,10 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb > 20 || nb <= 0 {
+	if nb > 20 || nb < 0 {
 		return 0
 	}
-	if nb == 1 {
+	if nb <= 1 {
 		return 1
 	} else {
 		return nb * RecursiveFactorial(nb-1)
