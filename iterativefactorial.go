21bf872cb02f3e138785bab21cd225879e8bee47
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 14:23:46 +0200

    find limits

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,7 +3,7 @@ package piscine
 func IterativeFactorial(nb int) int {
 	result := 0
 	nbtemp := nb - 1
-	for index := 1; index <= nbtemp; index++ {
+	for index := 1; index <= nbtemp && nbtemp <= 20; index++ {
 		nb *= index
 		result = nb
 	}
