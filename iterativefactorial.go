9c1684d4dd2ace4eb22069562d61a956348f93e9
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 13:54:39 +0200

    change limits

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,7 +2,7 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	for index := 1; index <= nb; index++ {
+	for index := 1; 184467440737095516 > result && index <= nb; index++ {
 		result *= index
 	}
 	if result == 1 {
