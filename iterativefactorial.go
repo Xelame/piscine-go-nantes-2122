0adc7d7b0aef7c8edcf4c6dd523590acc6b47dde
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 13:31:21 +0200

    Adjust my loop to think about error

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,9 +1,10 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	result := 1
-	for index := nb; index > 0; index-- {
-		result *= index
+	result := 0
+	for index := nb - 1; index > 0; index-- {
+		nb *= index
+		result = nb
 	}
 	return result
 }
