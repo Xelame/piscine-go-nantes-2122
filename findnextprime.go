34c3e4665204e7ae8d8f5c27648f75c12099c87c
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 23:21:41 +0200

    sqrt work just think back

--- findnextprime.go
+++ findnextprime.go
@@ -3,11 +3,9 @@ package piscine
 func FindNextPrime(nb int) int {
 	if nb <= 1 {
 		return 2
-	} else if nb == 4 {
-		return 5
 	}
 	isPrime := true
-	for diviser := nb - Sqrt(nb) - 1; diviser > 1 && isPrime; diviser-- {
+	for diviser := 2; diviser*diviser <= nb && isPrime; diviser++ {
 		if nb%diviser == 0 {
 			isPrime = false
 		}
