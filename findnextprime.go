7caf3c87cec152be5f6dbb5ba30b5938ad78d629
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 16:42:11 +0200

    EX 7 Q4

---
+++ findnextprime.go
@@ -0,0 +1,24 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	result := 0
+	countDivideBy := 0
+	var listOfPrimeNumber []int
+	for number := 1; number < nb*10; number++ {
+		countDivideBy = 0
+		for diviser := 1; diviser <= number; diviser++ {
+			if number%diviser == 0 {
+				countDivideBy += 1
+			}
+		}
+		if countDivideBy == 2 {
+			listOfPrimeNumber = append(listOfPrimeNumber, number)
+		}
+	}
+	for _, i := range listOfPrimeNumber {
+		if i <= nb {
+			result = i
+		}
+	}
+	return result
+}
