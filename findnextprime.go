c0362be8bf1d398d3317ebc1dbce1abf8a4a831a
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 22:28:28 +0200

    adjust limits

--- findnextprime.go
+++ findnextprime.go
@@ -1,16 +1,16 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb <= 0 {
+	if nb <= 1 {
 		return 2
 	}
 	countDivideBy := 0
-	for diviser := 1; diviser <= nb && countDivideBy <= 2; diviser++ {
+	for diviser := 2; diviser < nb && countDivideBy <= 2; diviser++ {
 		if nb%diviser == 0 {
 			countDivideBy += 1
 		}
 	}
-	if countDivideBy == 2 {
+	if countDivideBy == 0 {
 		return nb
 	} else {
 		return FindNextPrime(nb + 1)
