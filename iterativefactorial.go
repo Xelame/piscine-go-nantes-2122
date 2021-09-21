7eb2089ddd75fb8adb904041304a85df2eeece2c
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 14:27:58 +0200

    ex 1 q4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	result := 0
-	nbtemp := nb - 1
-	for index := 1; index <= nbtemp && nbtemp <= 20; index++ {
-		nb *= index
+	result := 1
+	for index := 1; index <= nb && nb <= 20; index++ {
+		result *= index
 		result = nb
+	}
+	if result == 1 {
+		return 0
 	}
 	return result
 }
