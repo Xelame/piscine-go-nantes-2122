5ce87836f4d9f74751a2c528a9e9dd719f116306
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 14:02:07 +0200

    reorder

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,14 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
+	if nb > 1000000000000000000 {
+		return 0
+	}
 	result := 1
-	for index := 1; 9223372036854775806 > result && index <= nb; index++ {
+	for index := 1; 1000000000000000000 > result && index <= nb; index++ {
 		result *= index
 	}
-	if result == 1 || nb > 9223372036854775806 {
+	if result == 1 {
 		return 0
 	}
 	return result
