0069c579434c60a13654dde8134b101791504502
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 13:56:49 +0200

    add condition

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,7 +5,7 @@ func IterativeFactorial(nb int) int {
 	for index := 1; 184467440737095516 > result && index <= nb; index++ {
 		result *= index
 	}
-	if result == 1 {
+	if result == 1 || nb > 184467440737095516 {
 		return 0
 	}
 	return result
