426ff546b2cf5d2aada86ceeaf572f789c79c0f2
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-21 14:35:23 +0200

    remove useless line

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,12 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	result := 1
 	if nb > 20 || nb < 0 {
 		return 0
 	}
+	result := 1
 	for index := 1; index <= nb; index++ {
 		result *= index
-		result = nb
 	}
 	return result
 }
