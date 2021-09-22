8137f11f5e00626e57d1c41e95f447021ed1e272
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-22 20:19:39 +0200

    EX 20 Q5

---
+++ printnbrbase.go
@@ -0,0 +1,36 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbrBase(number int, base string) {
+	isOkay := true
+	listOfNumberInMyBase := []rune(base)
+	for i := len(listOfNumberInMyBase) - 1; i > 0; i-- {
+		if listOfNumberInMyBase[i] == listOfNumberInMyBase[i-1] {
+			z01.PrintRune('N')
+			z01.PrintRune('V')
+			isOkay = false
+		}
+	}
+	if isOkay {
+		// Test if number is negative
+		if number < 0 {
+			z01.PrintRune('-')
+		}
+		var reversedNumberInList []int
+		for number >= len(base) || number <= -len(base) {
+			reversedNumberInList = append(reversedNumberInList, number%len(base))
+			number /= len(base)
+		}
+		reversedNumberInList = append(reversedNumberInList, number%len(base))
+		for end := len(reversedNumberInList) - 1; end >= 0; end-- {
+			if reversedNumberInList[end] > 0 {
+				z01.PrintRune(listOfNumberInMyBase[reversedNumberInList[end]%48])
+			} else {
+				z01.PrintRune(listOfNumberInMyBase[-(reversedNumberInList[end] % 48)])
+			}
+		}
+	}
+}
