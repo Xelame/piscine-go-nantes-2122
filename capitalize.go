d757ed863e2207a180e2172b90b383ba608516c2
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-22 17:40:47 +0200

    adjust condition

---
+++ capitalize.go
@@ -0,0 +1,20 @@
+package piscine
+
+import "fmt"
+
+func Capitalize(aString string) string {
+	aString = ToLower(aString)
+	listOfCharacter := []rune(aString)
+	if IsLower(string(listOfCharacter[0])) {
+		listOfCharacter[0] -= 32
+	}
+	for i := 1; i < len(listOfCharacter); i++ {
+		if 63 >= listOfCharacter[i-1] && listOfCharacter[i-1] >= 32 {
+			fmt.Println(string(listOfCharacter[i]))
+			if IsLower(string(listOfCharacter[i])) {
+				listOfCharacter[i] -= 32
+			}
+		}
+	}
+	return string(listOfCharacter)
+}
