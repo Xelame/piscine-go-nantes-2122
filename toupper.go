fe24f30c2c7fab0f2e46240e89f32f25818caf6a
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-22 15:44:09 +0200

    EX 13 Q5

--- toupper.go
+++ toupper.go
@@ -3,8 +3,8 @@ package piscine
 func ToUpper(aString string) string {
 	listOfCharacter := []rune(aString)
 	for _, value := range listOfCharacter {
-		if 65 <= value && value <= 90 {
-			value += 32
+		if 97 <= value && value <= 122 {
+			value -= 32
 		}
 	}
 	return string(listOfCharacter)
