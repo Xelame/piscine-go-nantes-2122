b8aa9ab18997a8162772c5ac85a5975a77e58a3e
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-22 14:51:43 +0200

    change condition

--- isalpha.go
+++ isalpha.go
@@ -3,7 +3,7 @@ package piscine
 func IsAlpha(aString string) bool {
 	isAlpha := true
 	for _, value := range aString {
-		if ((65 <= int(value) && int(value) <= 90) || (97 <= int(value) && int(value) <= 122)) && isAlpha {
+		if !((65 <= int(value) && int(value) <= 90) || (97 <= int(value) && int(value) <= 122)) && isAlpha {
 			isAlpha = !isAlpha
 		}
 	}
