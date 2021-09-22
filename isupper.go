2867eb4d5b9c3215fe7bd98ffe733224bb9f919d
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-22 14:23:56 +0200

    add condition

--- isupper.go
+++ isupper.go
@@ -3,7 +3,7 @@ package piscine
 func IsUpper(aString string) bool {
 	isUpper := true
 	for _, value := range aString {
-		if !(65 <= int(value) && int(value) <= 90) {
+		if !(65 <= int(value) && int(value) <= 90) && isUpper {
 			isUpper = !isUpper
 		}
 	}
