1a1e1a32e76eb173531311cfd6b7c6b1c507aa1e
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-22 15:20:49 +0200

    EX 13 Q5

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(aString string) bool {
 	isPrint := true
 	for _, value := range aString {
-		if !(0 < int(value) && int(value) < 20) && isPrint {
+		if (0 <= int(value) && int(value) <= 30) && isPrint {
 			isPrint = !isPrint
 		}
 	}
