605425a22bd1bd24fc55949874433f095f8cbc42
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 15:18:17 +0200

    adjust to working

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,9 +9,11 @@ import (
 func main() {
 	fichier := os.Args[0]
 	begin := 0
+	isFirst := true
 	for i := len(fichier) - 1; i >= 0; i-- {
-		if fichier[i] == '/' {
+		if fichier[i] == '/' && isFirst {
 			begin = i + 1
+			isFirst = !isFirst
 		}
 	}
 	for begin < len(fichier) {
