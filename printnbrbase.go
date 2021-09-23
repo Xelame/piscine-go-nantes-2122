6accfd8e822a95f439c826ac7eada788af0d4a43
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 18:32:35 +0200

    adapt my code by this before

--- printnbrbase.go
+++ printnbrbase.go
@@ -5,33 +5,38 @@ import (
 )
 
 func PrintNbrBase(number int, base string) {
+	if TestBase(base) {
+		if number == -9223372036854775808 {
+			number = 223372036854775808
+			z01.PrintRune('-')
+			z01.PrintRune('9')
+		}
+		if number < 0 {
+			z01.PrintRune('-')
+			number = -number
+		}
+		var reversedList []int
+		for number >= len(base) {
+			reversedList = append(reversedList, number%len(base))
+			number /= len(base)
+		}
+		reversedList = append(reversedList, number%len(base))
+		for end := len(reversedList) - 1; end >= 0; end-- {
+			z01.PrintRune(rune(base[reversedList[end]]))
+		}
+	}
+}
+
+func TestBase(base string) bool {
 	isOkay := true
-	listOfNumberInMyBase := []rune(base)
-	for i := len(listOfNumberInMyBase) - 1; i > 0; i-- {
+	for i := len(base) - 1; i > 0; i-- {
 		for j := i - 1; j >= 0; j-- {
-			if (listOfNumberInMyBase[i] == listOfNumberInMyBase[j]) && listOfNumberInMyBase[j] != 45 && listOfNumberInMyBase[j] != 43 && isOkay && len(listOfNumberInMyBase) < 38 {
+			if (base[i] == base[j]) && base[j] != 45 && base[j] != 43 && isOkay {
 				z01.PrintRune('N')
 				z01.PrintRune('V')
 				isOkay = false
 			}
 		}
 	}
-	if isOkay {
-		if number < 0 {
-			z01.PrintRune('-')
-		}
-		var reversedNumberInList []int
-		for number >= len(base) || number <= -len(base) {
-			reversedNumberInList = append(reversedNumberInList, number%len(base))
-			number /= len(base)
-		}
-		reversedNumberInList = append(reversedNumberInList, number%len(base))
-		for end := len(reversedNumberInList) - 1; end >= 0; end-- {
-			if reversedNumberInList[end] > 0 {
-				z01.PrintRune(listOfNumberInMyBase[reversedNumberInList[end]%48])
-			} else {
-				z01.PrintRune(listOfNumberInMyBase[-(reversedNumberInList[end] % 48)])
-			}
-		}
-	}
+	return isOkay
 }
