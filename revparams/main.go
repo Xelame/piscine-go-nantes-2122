241d4715fc2b913e545f91bba30375b6b4ae5b06
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 16:03:27 +0200

    EX 3 Q6

---
+++ revparams/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	fichier := os.Args
+	revfichier := []string{}
+	for i := len(fichier) - 1; i > 1; i-- {
+		revfichier = append(revfichier, fichier[i])
+	}
+	for _, arg := range revfichier {
+		begin := 0
+		isFirst := true
+		for i := len(arg) - 1; i >= 0; i-- {
+			if arg[i] == '/' && isFirst {
+				begin = i - 1
+				isFirst = !isFirst
+			}
+		}
+		for i, value := range arg {
+			if i >= begin {
+				z01.PrintRune(value)
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
