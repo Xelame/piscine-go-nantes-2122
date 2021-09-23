cca882dc3b8e265337a8ce2a0fdcf1bf0bf2a3e9
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 14:28:28 +0200

    EX 1 Q6

---
+++ printprogramname/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	fichier := os.Args
+	for i, strValue := range fichier {
+		if i == len(fichier)-1 {
+			for _, runeValue := range strValue {
+				if rune(runeValue) != '/' {
+					z01.PrintRune(rune(runeValue))
+				}
+			}
+		}
+
+	}
+}
