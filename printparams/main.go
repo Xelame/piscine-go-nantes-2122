6fa1a584a6ca143e586527074d86ffc4bdc671d7
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 15:49:01 +0200

    EX 2 Q6

---
+++ printparams/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	fichier := os.Args
+	for _, arg := range fichier {
+		begin := 0
+		isFirst := true
+		for i := len(arg) - 1; i >= 0; i-- {
+			if arg[i] == '/' && isFirst {
+				begin = i
+				isFirst = !isFirst
+			}
+		}
+		for i, value := range arg {
+			if i > begin {
+				z01.PrintRune(value)
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
