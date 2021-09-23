3c1cd67304de7f29a3e9adbe79fae1a67649824a
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 17:36:16 +0200

    EX 5 Q6

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -8,20 +8,27 @@ import (
 
 func main() {
 	fichier := os.Args
+	listOfInt := []int{}
+	var converter rune = 96
 	for _, arg := range fichier[1:] {
-		begin := 0
-		isFirst := true
-		for i := len(arg) - 1; i >= 0; i-- {
-			if arg[i] == '/' && isFirst {
-				begin = i - 1
-				isFirst = !isFirst
-			}
-		}
-		for i, value := range arg {
-			if i >= begin {
-				z01.PrintRune(value + 47)
-			}
+		if arg == "--upper" {
+			converter = 64
+		} else {
+			listOfInt = append(listOfInt, BasicAtoi(arg))
 		}
 	}
-	z01.PrintRune('\n')
+	for _, value := range listOfInt {
+		z01.PrintRune(rune(value + int(converter)))
+		z01.PrintRune('\n')
+	}
+}
+
+func BasicAtoi(aString string) int {
+	powerOfTen := 1
+	result := 0
+	for index := range aString {
+		result += powerOfTen * int(aString[len(aString)-1-index]%48)
+		powerOfTen *= 10
+	}
+	return result
 }
