75f19a095eeac48f1c6daf253b4deeebd9628210
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 18:35:42 +0200

    test maxime prog

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,4 +1,4 @@
-package piscine
+/*package piscine
 
 import (
 	"github.com/01-edu/z01"
@@ -38,4 +38,49 @@ func TestBase(base string) bool {
 		}
 	}
 	return isOkay
+}
+*/
+
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func TestBase(s string) bool {
+	runes := []rune(s)
+	if runes[len(runes)-1] == '-' {
+		return true
+	}
+	for i := 0; i < len(runes)-1; i++ {
+		for j := i + 1; j < len(runes); j++ {
+			if runes[i] == runes[j] || runes[i] == '-' {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func PrintNbrBase(nbr int, base string) {
+	if len(base) <= 1 || (TestBase(base)) {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+	} else {
+		if nbr == -9223372036854775808 {
+			nbr = 223372036854775808
+			z01.PrintRune('-')
+		} else if nbr < 0 {
+			nbr *= -1
+			z01.PrintRune('-')
+		}
+		var ints []int
+		for nbr > 0 {
+			ints = append(ints, nbr%len(base))
+			nbr = int(nbr / len(base))
+		}
+		for i := len(ints) - 1; i > -1; i-- {
+			z01.PrintRune(rune(base[ints[i]]))
+		}
+	}
 }
