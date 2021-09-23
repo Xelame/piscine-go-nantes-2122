7ec2375979f1e910882289c206c4aa3692a624de
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-23 19:00:07 +0200

    more simply

--- revparams/main.go
+++ revparams/main.go
@@ -7,24 +7,14 @@ import (
 )
 
 func main() {
-	fichier := os.Args
+	fichier := os.Args[1:]
 	revfichier := []string{}
-	for i := len(fichier) - 1; i > 0; i-- {
+	for i := len(fichier) - 1; i >= 0; i-- {
 		revfichier = append(revfichier, fichier[i])
 	}
 	for _, arg := range revfichier {
-		begin := 0
-		isFirst := true
-		for i := len(arg) - 1; i >= 0; i-- {
-			if arg[i] == '/' && isFirst {
-				begin = i - 1
-				isFirst = !isFirst
-			}
-		}
-		for i, value := range arg {
-			if i >= begin {
-				z01.PrintRune(value)
-			}
+		for _, value := range arg {
+			z01.PrintRune(value)
 		}
 		z01.PrintRune('\n')
 	}
