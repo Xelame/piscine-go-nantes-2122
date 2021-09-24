6d4a20a575901811ffbd0f294189833d4303528d
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-24 16:24:55 +0200

    EX bonus Q7

---
+++ convertbase.go
@@ -0,0 +1,25 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func ConvertBase(number, baseFrom, baseTo string) string {
+	result := []rune{}
+	nbr := AtoiBase(number, baseFrom)
+	if nbr == -9223372036854775808 {
+		nbr = 223372036854775808
+		z01.PrintRune('-')
+		z01.PrintRune('9')
+	} else if nbr < 0 {
+		nbr *= -1
+		result = append(result, '-')
+	}
+	var ints []int
+	for nbr > 0 {
+		ints = append(ints, nbr%len(baseTo))
+		nbr = int(nbr / len(baseTo))
+	}
+	for i := len(ints) - 1; i > -1; i-- {
+		result = append(result, rune(baseTo[ints[i]]))
+	}
+	return string(result)
+}
