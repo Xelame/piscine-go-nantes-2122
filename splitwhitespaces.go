8486d4940a959a9187586aed73fc7ab87d4b11b3
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-24 14:12:01 +0200

    fisrst is whitespace

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -5,7 +5,9 @@ func SplitWhiteSpaces(s string) []string {
 	begin := -1
 	for index, value := range s {
 		if value == ' ' || value == '\n' || value == '\t' {
-			arg = append(arg, s[begin+1:index])
+			if s[begin+1:index] != "" {
+				arg = append(arg, s[begin+1:index])
+			}
 			begin = index
 		}
 	}
