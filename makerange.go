81e46f43cf277ac7e4b3359c245b28c3bc9615dc
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-24 11:02:05 +0200

    EX 2 Q7

---
+++ makerange.go
@@ -0,0 +1,12 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+	result := []int{max - min}
+	for i := 0; i < len(result); i++ {
+		result[i] = min + i
+	}
+	return result
+}
