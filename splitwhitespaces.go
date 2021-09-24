ff4bcf2fd4deaedf0b489da3d1f7a3c9d20d2af7
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-24 14:09:30 +0200

    remove white

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -2,13 +2,13 @@ package piscine
 
 func SplitWhiteSpaces(s string) []string {
 	arg := []string{}
-	begin := 0
+	begin := -1
 	for index, value := range s {
 		if value == ' ' || value == '\n' || value == '\t' {
-			arg = append(arg, s[begin:index])
+			arg = append(arg, s[begin+1:index])
 			begin = index
 		}
 	}
-	arg = append(arg, s[begin:])
+	arg = append(arg, s[begin+1:])
 	return arg
 }
