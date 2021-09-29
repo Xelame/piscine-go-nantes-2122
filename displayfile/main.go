89205b79ed91dfc5c88715ce81e20144f9e00bb5
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-29 23:33:37 +0200

    update

--- displayfile/main.go
+++ displayfile/main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
 	if len(os.Args[1:]) == 1 {
 		if !(len(os.Args[1:]) > 1) {
-			file, err := os.Open(os.Args[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-			fi, err := file.Stat()
-			if err != nil {
-				log.Fatal(err)
-			}
+			file, _ := os.Open(os.Args[1])
+			fi, _ := file.Stat()
 			arr := make([]byte, fi.Size())
 			file.Read(arr)
-			fmt.Println(string(arr))
+			fmt.Print(string(arr))
 		} else {
 			fmt.Println("Too many arguments")
 		}
