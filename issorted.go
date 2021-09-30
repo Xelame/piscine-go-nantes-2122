5ea5278574b28563d5701d04e1da59a9d4fdd286
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-30 14:21:41 +0200

    EX 5 Q9

--- issorted.go
+++ issorted.go
@@ -3,14 +3,21 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	test := []int{}
 	isOkay := false
-	count := 0
+	count1 := 0
+	count2 := 0
 	for i := 0; i < len(a)-1; i++ {
 		test = append(test, f(a[i], a[i+1]))
 	}
 	for _, value := range test {
 		if value == 0 {
-			count++
-			if count == len(test) {
+			count1++
+			if count1 == len(test) {
+				isOkay = true
+			}
+		}
+		if value > 0 {
+			count2++
+			if count2 == len(test) {
 				isOkay = true
 			}
 		}
