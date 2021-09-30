f6022f88e162e3bc0dc910d84c4253d873913d47
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-30 14:28:49 +0200

    ty yassin v2

--- issorted.go
+++ issorted.go
@@ -5,10 +5,7 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	isOkay := false
 	count := 0
 	for i := 0; i < len(a)-1; i++ {
-		test = append(test, f(a[i], a[i+1]))
-	}
-	for _, value := range test {
-		if value == 0 {
+		if f(a[i], a[i+1]) > 0 {
 			count++
 		} else {
 			count--
