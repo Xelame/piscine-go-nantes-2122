ffae556d6c8351caacc7adf65a64ab6f2450de3b
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-30 13:15:31 +0200

    test

--- cat/main.go
+++ cat/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"io/ioutil"
 	"os"
 
@@ -10,20 +9,31 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
-			panic(err)
+		c, e := ioutil.ReadAll(os.Stdin)
+		if e == nil {
+			printStr(string(c))
+		} else {
+			printStr(e.Error())
 		}
 	} else {
 		for _, arg := range os.Args[1:] {
-			_, err := ioutil.ReadFile(arg)
+			b, err := ioutil.ReadFile(arg)
 			if err != nil {
 				errorMessage := "ERROR: " + err.Error()
 				for _, letter := range errorMessage {
 					z01.PrintRune(letter)
 				}
 				z01.PrintRune('\n')
+				os.Exit(1)
 			}
-			os.Exit(1)
+			printStr(string(b))
 		}
 	}
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
