4af3ca7d85807fe6628e7c91470f6dca318c5716
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-30 09:21:27 +0200

    test q8

--- cat/main.go
+++ cat/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	if !(len(os.Args[1:]) < 2) {
+		file1, _ := os.Open(os.Args[1])
+		fi1, _ := file1.Stat()
+		file2, _ := os.Open(os.Args[2])
+		fi2, _ := file2.Stat()
+		arr1 := make([]byte, fi1.Size())
+		arr2 := make([]byte, fi2.Size())
+		file1.Read(arr1)
+		file2.Read(arr2)
+		fmt.Print(string(arr1))
+		fmt.Print(string(arr2))
+	} else {
+		fmt.Println("File name missing")
+	}
 }
