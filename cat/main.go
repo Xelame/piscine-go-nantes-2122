7d2beaae26f5fed6a382b141eb71c506bd03059a
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-09-30 09:57:03 +0200

    test error

--- cat/main.go
+++ cat/main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	if !(len(os.Args[1:]) < 2) {
+	if len(os.Args[1:]) <= 2 {
 		file1, _ := os.Open(os.Args[1])
 		fi1, _ := file1.Stat()
+		arr1 := make([]byte, fi1.Size())
+		file1.Read(arr1)
+		for _, value := range arr1 {
+			z01.PrintRune(rune(value))
+		}
+	}
+	if len(os.Args[1:]) == 2 {
 		file2, _ := os.Open(os.Args[2])
 		fi2, _ := file2.Stat()
-		arr1 := make([]byte, fi1.Size())
 		arr2 := make([]byte, fi2.Size())
-		file1.Read(arr1)
 		file2.Read(arr2)
-		fmt.Print(string(arr1))
-		fmt.Print(string(arr2))
-	} else {
-		fmt.Println("File name missing")
+		for _, value := range arr2 {
+			z01.PrintRune(rune(value))
+		}
 	}
 }
