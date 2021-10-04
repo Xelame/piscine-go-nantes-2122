caa586023ea3e4859054d60516d589ec84a53762
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-10-04 15:59:43 +0200

    Ex 13 Q11

--- listmerge.go
+++ listmerge.go
@@ -15,5 +15,9 @@ type List struct {
 func ListMerge(l1 *List, l2 *List) {
 	if l1.Head != nil && l2.Head != nil {
 		l1.Tail.Next = l2.Head
+	} else if l2.Head != nil {
+		l1.Head = l2.Head
+	} else {
+		l2.Head = l1.Head
 	}
 }
