4e1d7b6cbc42cffe9b739a7133442e061434e49d
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-10-04 09:16:07 +0200

    Ex 4 Q11

---
+++ listlast.go
@@ -0,0 +1,17 @@
+package piscine
+
+/*
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+*/
+
+func ListLast(l *List) interface{} {
+	return l.Tail
+}
