bf19c7d27b318b838403be49c50251803eb30941
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-10-04 11:41:19 +0200

    Ex 7 Q11

---
+++ listforeach.go
@@ -0,0 +1,39 @@
+package piscine
+
+/*
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+*/
+
+func ListForEach(l *List, f func(*NodeL)) {
+	var currentnode *NodeL = l.Head
+	if l.Head != nil && currentnode.Next != nil {
+		f(currentnode)
+		currentnode = currentnode.Next
+	}
+}
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
