c7d0abf435258eaf4c7968c751af63d744bab30a
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-10-04 22:03:14 +0200

    test

--- listsort.go
+++ listsort.go
@@ -6,21 +6,23 @@ type NodeI struct {
 }
 
 func ListSort(l *NodeI) *NodeI {
-	if l != nil {
-		var currentnode *NodeI = l
-		var previous *NodeI = nil
-		var forward *NodeI
-		for currentnode.Next != nil {
-			forward = currentnode.Next
-			if currentnode.Data > forward.Data {
-				currentnode.Next = previous
-				previous = currentnode
-				currentnode = forward
-			}
-		}
-		currentnode.Next = previous
-		return currentnode
-	} else {
+	if l == nil {
 		return nil
 	}
+
+	var currentnode *NodeI = l
+	var previous *NodeI = nil
+	var forward *NodeI
+	for currentnode.Next != nil {
+		forward = currentnode.Next
+		if currentnode.Data > forward.Data {
+			currentnode.Next = previous
+			previous = currentnode
+			currentnode = forward
+		} else {
+			currentnode = currentnode.Next
+		}
+	}
+	currentnode.Next = previous
+	return currentnode
 }
