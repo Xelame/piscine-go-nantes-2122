074fa83b3cc05006f7bd68032b9ee04bfc5ced34
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-10-04 15:51:05 +0200

    Ex 12 Q11

---
+++ listmerge.go
@@ -0,0 +1,17 @@
+package piscine
+
+/*
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+*/
+
+func ListMerge(l1 *List, l2 *List) {
+	l1.Tail.Next = l2.Head
+}
