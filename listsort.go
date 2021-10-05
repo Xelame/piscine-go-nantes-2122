0067cf4d07597c6f04a6923a1e9bcca6bab72b62
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-10-05 09:11:49 +0200

    add comment

--- listsort.go
+++ listsort.go
@@ -8,27 +8,33 @@ type NodeI struct {
 // A commenter
 func ListSort(l *NodeI) *NodeI {
 	head := l
+	// Condition d'arret
 	if head == nil {
 		return nil
 	}
+	// Récursivité pour allez jusqu'au dernier element
 	head.Next = ListSort(head.Next)
-
+	// + empile la suite de notre programme pour retourner au debut
 	if head.Next != nil && head.Data > head.Next.Data {
-		head = move(head)
+		// Si l'élément d'avant est plus grand que celui d'apres on les interverties (leur pointage)
+		head = Move(head)
 	}
+	// Retourne l'element le plus petit des 2
 	return head
 }
 
-func move(l *NodeI) *NodeI {
-	p := l
-	n := l.Next
-	ret := n
-
-	for n != nil && l.Data > n.Data {
-		p = n
-		n = n.Next
+// Fonction pour swap 2 element et réarranger leur pointage
+func Move(node *NodeI) *NodeI {
+	// init nos deux element
+	previous := node
+	current := node.Next
+	result := current
+	// rearrange nos valeur jusqu'a ce que la valeur de base soit plus grande que sa précédente
+	for current != nil && node.Data > current.Data {
+		previous = current
+		current = current.Next
 	}
-	p.Next = l
-	l.Next = n
-	return ret
+	previous.Next = node
+	node.Next = current
+	return result
 }
